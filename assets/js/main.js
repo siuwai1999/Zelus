1e0e7381a75eb7db41d37a540f1f559bae63db30
SIU WAI KWAN <SIUWAI1999@outlook.com>
2023-03-14 03:56:10 +0800

    0314

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,24 @@ $(document).ready(function() {
 		prevEl: ".swiper-button-prev",
 		},
 		});
+	var swiper = new Swiper(".GamesceneSwiper", {
+		spaceBetween: 30,
+		lazy: true,
+		loop: true,
+		centeredSlides: true,
+		autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+		},
+		pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		},
+		navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+		},
+		});
 	});
 
 (function($) {
